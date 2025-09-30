5c93c04ac85be29aee3e0e6a52bfa71e48d1deb8
mohayminul-bd <mohayminulislam514@gmail.com>
2025-09-30 11:44:12 +0600

    rout recap

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -5,26 +5,26 @@ import PageLoader from "../components/PageLoader";
 import UseAuth from "../hooks/useAuth";
 
 const PrivateRoute = ({ children }) => {
-	const { user, loading } = UseAuth();
-	const location = useLocation();
-	const [showToast, setShowToast] = useState(false);
+  const { user, loading } = UseAuth();
+  const location = useLocation();
+  const [showToast, setShowToast] = useState(false);
 
-	useEffect(() => {
-		if (!loading && !user && !showToast) {
-			toast.error("You must be logged in to see the content.");
-			setShowToast(true);
-		}
-	}, [loading, user, showToast]);
+  useEffect(() => {
+    if (!loading && !user && !showToast) {
+      toast.error("You must be logged in to see the content");
+      setShowToast(true);
+    }
+  }, [loading, user, showToast]);
 
-	if (loading) {
-		return <PageLoader />;
-	}
+  if (loading) {
+    return <PageLoader />;
+  }
 
-	if (user && user?.email) {
-		return children;
-	}
+  if (user && user?.email) {
+    return children;
+  }
 
-	return <Navigate to="/login" state={{ from: location }} replace />;
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
